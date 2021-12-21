6f4aebba51d22c3a1d61b3e28741707a8ae48b8b
babsang781 <89438832+babsang781@users.noreply.github.com>
2021-12-21 22:50:03 +0900

    Create TestInput.py

---
+++ practiceCode/JupiterNote/TestInput.py
@@ -0,0 +1,19 @@
+# 1 
+
+num1 = int(input("입력>>"))
+num2 = int(input("입력>>"))
+print(num1*num2)
+print("\n더하기 결과 : %d"%(num1+num2))
+print("빼기 결과 : %d"%(num1-num2))
+print("곱하기 결과 : {}".format(num1*num2))
+print("나누기 결과 : %f"%(num1/num2))
+
+
+# 2
+
+py = int(input("python 점수 입력>> "))
+ml = int(input("머신러닝 점수 입력>> "))
+dl = int(input("딥러닝 점수 입력>> "))
+print("합계 : {}".format(py+ml+dl))
+print("평균 : {}".format((py+ml+dl)/3))
+
