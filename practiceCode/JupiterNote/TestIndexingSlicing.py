fdda378a00332120d2e6afe8f731d65dd33704ea
babsang781 <89438832+babsang781@users.noreply.github.com>
2021-12-21 22:45:44 +0900

    Create TestIndexingSlicing.py

---
+++ practiceCode/JupiterNote/TestIndexingSlicing.py
@@ -0,0 +1,19 @@
+s="My name is BM"
+name = """My name is
+.
+.
+.
+Sam"""
+print(name[0]+name[1])
+print(name[0]+ name[1])
+
+print(name[-4]+ "_"+ name[4]+ "_"+name[8]+ "_"+name[10]+ "_"+name[11]+ "_"+name[12]+"_"+name[13]+"_"+name[14]+"_"+name[15])
+print(name[10:15])
+
+
+#Sam 가져오기
+print(name[17:19],) 
+print(name[17:20]) # 끝나는 위치 인덱스 앞까지 가져옴
+print(name[-4:])
+print()
+print(name[:])
