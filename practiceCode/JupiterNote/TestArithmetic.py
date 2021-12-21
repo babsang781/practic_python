94a5785063e3f68536efee6ab9627b2d4fbcbade
babsang781 <89438832+babsang781@users.noreply.github.com>
2021-12-21 22:48:23 +0900

    Create TestArithmetic.py

---
+++ practiceCode/JupiterNote/TestArithmetic.py
@@ -0,0 +1,28 @@
+# 1
+str1 = "안녕"
+str2 = "하세요"
+print(str1+str2)
+print(str1,str2)
+print(str1+str2*2)
+print(str1*3,str2)
+print(str1*num3,str2)
+
+
+# 2 
+
+str5= "10"
+num3 = 7 
+print(int(str5) + num3)
+print(float(str5) + num3)
+print( str5 + str(num3))
+print( str5 + chr(100))
+
+
+# 3
+
+num1 , num2 = 23, 3
+print("더하기 결과 : %d"%(num1+num2))
+print("빼기 결과 : %d"%(num1-num2))
+print("곱하기 결과 : %d"%(num1*num2))
+print("나누기 결과 : %f"%(num1/num2))
+print(num1/num2)
